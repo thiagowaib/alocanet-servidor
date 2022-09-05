74750d4d73966514ef0d60beb75f29778e683fe0
thiagowaib <thiagowaib@gmail.com>
2022-09-05 13:43:49 -0300

    [New] ControllerAdmin Endpoints

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,18 @@ const express = require('express')
 const routes = new express.Router()
 
 // * Definição dos Endpoints
-/* 
-const {myMethod} = require('./controllers/example-controller')
-routes.get('/getExample/:id', myMethod) 
-*/
-
-
+// ControllerAdmin Endpoints
+const {novoAdmin, loginAdmin, updateAdminById, removeAdminById} = require('./controllers')
+const {AuthTokenAcesso} = require('./middlewares')
+routes.post('/novoAdmin', novoAdmin)
+routes.post('/loginAdmin', loginAdmin)
+routes.put('/updateAdmin/:id', updateAdminById)
+routes.delete('/removeAdmin/:id', removeAdminById)
+routes.get('/testAuth', AuthTokenAcesso, (req, res)=>{
+    res.status(200).send({
+        message:"JWT Validado",
+        admin: req.payload.usuario
+    })
+})
 // * Exportação das rotas para main.js
 module.exports = routes
