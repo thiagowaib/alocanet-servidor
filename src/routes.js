b366f6d817dac6dad9e000a36117087b1077d153
thiagowaib <thiagowaib@gmail.com>
2022-09-06 00:04:28 -0300

    [New] Endpoints CRUD Espaços

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ routes.post('/loginAdmin', loginAdmin)
 routes.put('/updateAdmin/:id', updateAdminById)
 routes.delete('/removeAdmin/:id', removeAdminById)
 
-// ControllerApartamentos Endpoits
+// ControllerApartamentos Endpoints
 const {novoApto, loginApto, modificarSenha, removerApto, consultarApto} = require('./controllers')
 routes.post('/novoApto', AuthTokenAcesso, novoApto)
 routes.post('/loginApto', loginApto)
@@ -24,6 +24,13 @@ routes.get('/consultarApto/:numero', AuthTokenAcesso, consultarApto) //! Não Fi
 routes.delete('/removerApto/:numero', AuthTokenAcesso, removerApto)
 // routes.put('/addLocacao/:numero', AuthTokenAcesso, addLocacao)
 // routes.put('/cancelarLocacao/:numero', AuthTokenAcesso, cancelarLocacao) //! Não Finalizado
+
+// ControllerEspacos Endpoints
+const {novoEspaco, buscarEspacos, modificarEspacoById, removerEspacoById} = require('./controllers')
+routes.post('/novoEspaco', AuthTokenAcesso, novoEspaco)
+routes.get('/buscarEspacos', AuthTokenAcesso, buscarEspacos)
+routes.put('/modificarEspaco/:id', AuthTokenAcesso, modificarEspacoById)
+routes.delete('/removerEspaco/:id', AuthTokenAcesso, removerEspacoById)
 
 // * Exportação das rotas para main.js
 module.exports = routes
