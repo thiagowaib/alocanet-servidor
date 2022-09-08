f952ed9f2dbca26ff73ae68c3a8723b72c61c362
thiagowaib <thiagowaib@gmail.com>
2022-09-08 13:57:29 -0300

    [New] Comentários

--- src/main.js
+++ src/main.js
@@ -18,14 +18,23 @@ const initServidor = () => {
         useNewUrlParser: true,
     })
 
+    /**
+     *  • Utilização de CORS para comunicação
+     *    entre protocólos HTTP e HTTPS
+     *  • Utilização de JSON como formatação
+     *    de dados nos métodos e endpoints
+     */
     app.use(express.json())
     app.use(cors())
 
     // Conexão com os endpoints em ./routes.js
     app.use(require('./routes'))
 
-    // Inicialização do servidor na porta {SERVER_PORT}
-    // Com feedback de confirmação
+    /**
+     * Inicialização do servidor na porta informada
+     * pelo arquivo de ambiente (.env) {SERVER_PORT}
+     * com feedback de confirmação
+     */
     const servidor = require('http').Server(app)
     servidor.listen(process.env.SERVER_PORT || 5000, () => {
         console.log(`Servidor AlocaNet online em ${process.env.SERVER_URL}:${process.env.SERVER_PORT}/`)
