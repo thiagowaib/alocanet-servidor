651789f2df8187aa59014ed1fbd20e8d22dc97ca
thiagowaib <thiagowaib@gmail.com>
2022-09-12 14:36:33 -0300

    [New] Mensagens de erro

--- src/middlewares/AuthTokenAcesso/index.js
+++ src/middlewares/AuthTokenAcesso/index.js
@@ -5,14 +5,14 @@ module.exports = (req, res, next) => {
   const token = req.headers['auth']
 
   if (token === null)
-    return res.sendStatus(401)
+    return res.status(401).send({message: "Informe o token de acesso no cabeçalho da request"})
 
   jwt.verify(
       token,
       process.env.JWT_ACCESS_TOKEN_SECRET,
       (err, payload) => {
-          if (err) return res.sendStatus(403)
-          if (payload.exp <= Date.now()) return res.sendStatus(410)
+          if (err) return res.status(403).send({message: "Token de acesso negado"})
+          if (payload.exp <= Date.now()) return res.status(410).send({message: "Token de acesso expirado"})
           req.payload = payload
           next()
       }
